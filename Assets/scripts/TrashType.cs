cd7c2c6a28bed84df0b0c3acd96dedccae971cf2
blaZe2603 <keshavb2603.24@kgpian.iitkgp.ac.in>
2025-08-17 14:55:00 +0530

    trash spwaner ready

---
+++ Assets/scripts/TrashType.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TrashType : MonoBehaviour
+{
+
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = GetRandomColor();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private Color GetRandomColor()
+    {
+        Color[] colors = { Color.red, Color.blue, Color.green, Color.yellow };
+        Color toRet = colors[Random.Range(0, colors.Length)];
+        if (toRet == Color.red)
+        {
+            gameObject.tag = "Hazardous Bin";
+        }
+        if (toRet == Color.blue)
+        {
+            gameObject.tag = "General Bin";
+        }
+        if (toRet == Color.green)
+        {
+            gameObject.tag = "Wet Bin";
+        }
+        if (toRet == Color.yellow)
+        {
+            gameObject.tag = "Recyclable Bin";
+        }
+        Debug.Log(gameObject.tag);
+        return toRet;
+    }
+}
