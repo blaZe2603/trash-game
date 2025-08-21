75ff4b6aea7a7a694afea1b80fb4ad5b54bfac9e
blaZe2603 <keshavb2603.24@kgpian.iitkgp.ac.in>
2025-08-21 16:28:49 +0530

    added audio and level end screen

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
